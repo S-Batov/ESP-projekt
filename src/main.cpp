7648a38163d7cde9d29053a8ab507b6e4e49f87c
Simun Batovanja <sbatov00@fesb.hr>
2022-12-26 22:32:21 +0100

    Added basic server behaviour

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <WiFiUdp.h>
 
 const char* ssid = "ISKONOVAC-7a7698";
@@ -14,21 +15,57 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
+//Define server
+ESP8266WebServer server(80);
+
+void handle_onConect(){
+  return;
+}
+void handle_setAlarm(){
+  return;
+}
+void handle_snooze(){
+  return;
+}
+void handle_stop(){
+  return;
+}
+void handle_badRequest(){
+  return;
+}
+
+
+
 void setup(){
   Serial.begin(115200);
+  
 
   WiFi.begin(ssid, password);
-
+  Serial.print("\n\n");
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
     Serial.print ( "." );
   }
+  Serial.print("\nConnected to WiFi: ");
+  Serial.println(ssid);
+  Serial.print("With IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_onConect);
+  server.on("/setAlarm", handle_setAlarm);
+  server.on("/snooze", handle_snooze);
+  server.on("/stop", handle_stop);
+  server.onNotFound(handle_badRequest);
+  
+  server.begin();
+  Serial.println("HTTP server started");
 
   timeClient.begin();
 }
 
 void loop() {
   timeClient.update();
+  server.handleClient();
 
   Serial.print(daysOfTheWeek[timeClient.getDay()]);
   Serial.print(", ");
