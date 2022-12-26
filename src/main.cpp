f9d93ec0f98d5dc74f47fe63845c6909bd35ffd8
Simun Batovanja <sbatov00@fesb.hr>
2022-12-27 00:30:38 +0100

    setAlarm reads parameters

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ const char* password = "ISKON2914504818";
 
 const long utcOffsetInSeconds = 3600;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const String daysOfTheWeek[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -17,12 +17,65 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 //Define server
 ESP8266WebServer server(80);
+void printLogAtTime(const char* msg){
+  Serial.print(msg);
+  Serial.print(" at: ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+}
+
+void setAlarmAt(uint8_t day, uint8_t hour, uint8_t minute, uint8_t second){
+  //TODO:
+  //Write body
+}
 
 void handle_onConect(){
+  //TODO:
+  //Send data about current alarm settings
+
+  printLogAtTime("Client connected");
+  server.send(200, "text/plain", "Hello world");
   return;
 }
 void handle_setAlarm(){
-  return;
+  // Parameter example
+  // http://website.com/path?parameter1=value1&parameter2=value2
+  printLogAtTime("Set alarm");
+
+  uint8_t argCount = server.args();
+  const uint8_t expectedArgNum = 4;
+  Serial.print("Number of arguments received: ");
+  Serial.println(argCount);
+  for(uint8_t i = 0; i < server.args(); i++){
+    String msg = (String)i + ": " + server.argName(i) + "=" + server.arg(i);
+    Serial.println(msg);
+  }
+
+  const String argNames[] = {"D", "H", "M", "S"};
+  switch (argCount){
+  case expectedArgNum:
+    for(uint8_t i = 0; i < expectedArgNum; i++){
+      if(argNames[i] != server.argName(i)){
+        String msg = "Expected parameter: \"" + argNames[i] + "\" but got: \"" + server.argName(i) + "\"";
+        Serial.println(msg);
+        server.send(400, "text/plain", "Wrong parameters");
+      }
+      //TODO:
+      //Provjerit je li vrijednost svakog parametra broj
+      //Ako je, postavit alarm
+    }
+    
+
+    server.send(200, "text/plain", "setAlarm");
+    return;
+  default:
+    server.send(400, "text/plain", "Expected 4 parameters: Day,Hour,Minute,Second");
+    Serial.print("Expected 4 parameters:\nDay,Hour,Minute,Second\n");
+    return;
+  }
 }
 void handle_snooze(){
   return;
@@ -67,6 +120,7 @@ void loop() {
   timeClient.update();
   server.handleClient();
 
+/*
   Serial.print(daysOfTheWeek[timeClient.getDay()]);
   Serial.print(", ");
   Serial.print(timeClient.getHours());
@@ -74,7 +128,8 @@ void loop() {
   Serial.print(timeClient.getMinutes());
   Serial.print(":");
   Serial.println(timeClient.getSeconds());
-  //Serial.println(timeClient.getFormattedTime());
-
+  Serial.println(timeClient.getFormattedTime());
+*/
+  
   delay(1000);
 }
